831cd3e3df06396f10c0ceeaa6b641d9cf0756e2
wenjus26 <rejuste.wenoumi@epitech.eu>
2024-05-05 21:53:20 +0100

    My seed quality management system

--- main.py
+++ main.py
@@ -1068,9 +1068,8 @@ def export_data_to_excel():
 
     return response
 
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run()
+    app.run(debug=True, port=5000)
 
